cafee4a0c8202ee9b585a3743f9627ebf136d797
Mateo Orteon <46364084+ThisIsTeo@users.noreply.github.com>
2021-01-18 23:28:12 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const socket = io('/')
+var PORT = process.env.PORT || 3001
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '/',
-  port: '3001'
+  port: 'PORT'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
